b6a8397b8363204025e3b8d107f640a6c31678bf
troliaoluatsu <thienan041803@gmail.com>
2022-10-20 14:01:42 +0700

    Finished

--- webthuexe-ui/website-thuexe/src/App.tsx
+++ webthuexe-ui/website-thuexe/src/App.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 // External file
-import axios from "axios";
 
 // Internal file
+import { Pokemon, Details } from "./interfaceGlobal";
 import PokemonCollection from "./components/PokemonCollection";
+import request from "./utils/request";
 
 // Styles
 import classNames from "classnames/bind";
 import styles from "./App.module.scss";
-import { Pokemon } from "./interfaceGlobal";
+import axios from "axios";
 const cx = classNames.bind(styles);
 
 interface Pokemons {
@@ -18,28 +19,60 @@ interface Pokemons {
 }
 
 const App: React.FC = () => {
+  // List Pokemon
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  // Load more Pokemon
+  const [nextUrl, setNextUrl] = useState<string>("");
+  // Loading
+  const [loading, setLoading] = useState<boolean>(true);
+  // Xem chi tiết
+  const [viewDetails, setViewDetails] = useState<Details>({
+    id: 0,
+    isOpened: false,
+  });
+
+  // Call API
   useEffect(() => {
     const getPokemon = async () => {
-      const res = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=20"
-      );
+      const res = await request.get("pokemon?limit=20&offset=20");
+
+      setNextUrl(res.data.next);
 
       res.data.results.forEach(async (pokemon: Pokemons) => {
-        const poke = await axios.get(
-          `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
-        );
+        const poke = await request.get(`pokemon/${pokemon.name}`);
         setPokemons((p) => [...p, poke.data]);
+        setLoading(false);
       });
     };
     getPokemon();
   }, []);
 
+  // Handle load more
+  const handleLoadMore = async () => {
+    setLoading(true);
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await request.get(`pokemon/${pokemon.name}`);
+      setPokemons((p) => [...p, poke.data]);
+      setLoading(false);
+    });
+  };
+
   return (
     <div className={cx("App")}>
       <div className={cx("container")}>
         <header className={cx("pokemon-header")}>Pokemon</header>
-        <PokemonCollection pokemons={pokemons} />
+        <PokemonCollection
+          pokemons={pokemons}
+          viewDetails={viewDetails}
+          setViewDetails={setViewDetails}
+        />
+        {!viewDetails.isOpened && (
+          <div className={cx("btn")} onClick={handleLoadMore}>
+            <button>{loading ? "Loading..." : "Load more"}</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- webthuexe-ui/website-thuexe/src/components/PokemonCollection/PokemonCollection.tsx
+++ webthuexe-ui/website-thuexe/src/components/PokemonCollection/PokemonCollection.tsx
@@ -1,27 +1,52 @@
 import React from "react";
 // External file
 // Internal file
-import { Pokemon } from "../../interfaceGlobal";
+import { Details, PokemonDetails } from "../../interfaceGlobal";
 import PokemonList from "./PokemonList";
 // Styles
 import classNames from "classnames/bind";
 import styles from "./PokemonCollection.module.scss";
 const cx = classNames.bind(styles);
 interface Props {
-  pokemons: Pokemon[];
+  pokemons: PokemonDetails[];
+  viewDetails: Details;
+  setViewDetails: React.Dispatch<React.SetStateAction<Details>>;
 }
 
 const PokemonCollection: React.FC<Props> = (props) => {
-  const { pokemons } = props;
+  const { pokemons, viewDetails, setViewDetails } = props;
+
+  const handleViewPokemon = (id: number) => {
+    if (!viewDetails.isOpened) {
+      setViewDetails({
+        id: id,
+        isOpened: true,
+      });
+    }
+  };
   return (
-    <section className={cx("collection-container")}>
+    <section
+      className={cx(
+        viewDetails.isOpened
+          ? "collection-container-active"
+          : "collection-container"
+      )}
+    >
+      {viewDetails.isOpened ? (
+        <div className={cx("overlay")}></div>
+      ) : (
+        <div className=""></div>
+      )}
       {pokemons.map((pokemon) => (
-        <div className={cx("")}>
+        <div className={cx("")} onClick={() => handleViewPokemon(pokemon.id)}>
           <PokemonList
             key={pokemon.id}
             name={pokemon.name}
             id={pokemon.id}
             image={pokemon.sprites.front_default}
+            abilities={pokemon.abilities}
+            viewDetails={viewDetails}
+            setViewDetails={setViewDetails}
           />
         </div>
       ))}
